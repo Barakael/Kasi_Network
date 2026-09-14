2e409cc1f3acfb909c2ea7778d12bd56d4cb2d56
barakael <barakaellucas2019@gmail.com>
2026-09-14 13:24:30 +0300

    Add portal routes

---
+++ api/routes/portal.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Controllers\Portal\BootstrapController;
+use App\Http\Controllers\Portal\DeviceController;
+use App\Http\Controllers\Portal\OrderController;
+use App\Http\Controllers\Portal\RedeemController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('bootstrap', BootstrapController::class)
+    ->middleware('throttle:30,1')
+    ->name('bootstrap');
+
+Route::middleware(['portal', 'throttle:60,1'])->group(function (): void {
+    Route::post('redeem', RedeemController::class)->name('redeem');
+    Route::post('orders', [OrderController::class, 'store'])->name('orders.store');
+    Route::get('orders/{order:uuid}', [OrderController::class, 'show'])->name('orders.show');
+    Route::get('devices/nearby', [DeviceController::class, 'nearby'])->name('devices.nearby');
+    Route::post('devices', [DeviceController::class, 'store'])->name('devices.store');
+});
